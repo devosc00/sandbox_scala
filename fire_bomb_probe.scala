7eec9f19b5f103808aa762756a24be29041979df
rafa <devosc00@gmail.com>
2013-08-20 15:53:07 +0200

    ???

--- fire_bomb_probe.scala
+++ fire_bomb_probe.scala
@@ -1,4 +1,4 @@
- import scala.util.Random
+import scala.util.Random
 
 val random = new Random
 case class Position(x: Int, y: Int)
@@ -10,7 +10,8 @@ case class Position(x: Int, y: Int)
  val data = random2DimArray(12, 15)
 
  var players = collection.mutable.Map.empty[String, Position]
- var pl = players.values.toList
+
+ def swap = players.map(_.swap)
  /*("p1" -> (1,0),
  	"p2"->(1,1), "p3"->(1,3), "p4"->(1,4), "p5"->(1,5),
  	"p2.2"->(2,1), "p3.2"->(2,3), "p4.2"->(2,4), "p5.2"->(2,5),
@@ -21,6 +22,8 @@ case class Position(x: Int, y: Int)
 
 def posList(pos: Position) = List[Position](pos)
 
+var pl = players.values.toList
+
 def randomPosition :Position = {
     val pos = new Position(random.nextInt(11), random.nextInt(14))
    if(data(pos.x)(pos.y).equals(1)) {
@@ -28,26 +31,50 @@ def randomPosition :Position = {
   } else randomPosition
 }
 
-	def addPlayer(user: String) = players += (user -> randomPosition)
+def addPlayer(user: String) = players += (user -> randomPosition)
 
-/*		def bomb(pos: Position) = {(list: List[Position])
-		   match {
-		  	case (pos.x > 1, pos.y > 1) => for{
-		  							i<-pos.x - 2 to pos.x + 2
-		  							j<-pos.y -2 to pos.y + 2
-		  							} yield list :+ list(pos)
-		  }
-		  
-		}*/
 
+def check (list: List[Position]) = {
+	if(!list.isEmpty){
+		val buffer = new collection.mutable.ListBuffer[String]()
+		for(lis <- list if pl.contains(lis)) {
+			buffer += swap(lis)
+		}
+		val li = buffer.toList
+		println(li)
+	}
+}
+
+
+def bomb(pos: Position) = { 
+	val buffer = new collection.mutable.ListBuffer[Position]
+	for (i <- pos.x - 2 to pos.x + 2){
+			buffer += Position(i , pos.y)
+
+			//println(listX)
+			//if(pl.contains(listX.head)) println("bingo x " + swap(listX.head))
+		}
+	for (i <- pos.y - 2 to pos.y + 2){
+			buffer += Position(pos.x , i)
+			//if(pl.contains(listY.head)) println("bingo y " + swap(listY.head))	
+		}
+		val list = buffer.distinct.toList
+		println(list)
+		check(list)
+}
+	
 def users = for(i <- 0 until 15) addPlayer("p" + i.toString)
-def li = players.values.toList
-def liDi = li.distinct
+
 users
+
+def li = pl
+def liDi = li.distinct
+bomb(Position(9,4))
+check(List(Position(9,4)))
 addPlayer("u")
+println(players)
 println(li)
 println(liDi)
-players.values.toList
-
+swap(Position(8,10))
 
 	
